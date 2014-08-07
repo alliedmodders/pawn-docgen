320eb059cd50befeea2ec48a6434d36720289f7a
xPaw <github@xpaw.me>
2014-08-07 21:31:36 +0300

    Make sidebar static width

--- www/template/footer.php
+++ www/template/footer.php
@@ -1,7 +1,7 @@
 <?php if( $RenderLayout ): ?>
 			</div>
 			
-			<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 footer">
+			<div class="col-lg-12 footer">
 				<p>This documentation was generated automatically using <a href="https://github.com/alliedmodders/pawn-docgen">pawn-docgen</a> written by <a href="//xpaw.me">xPaw</a> for <a href="//alliedmods.net/">AlliedMods</a>.</p>
 			</div>
 		</div>
--- www/template/header.php
+++ www/template/header.php
@@ -16,33 +16,34 @@
 	<link rel="stylesheet" href="<?php echo $BaseURL; ?>style.css">
 </head>
 <body data-baseurl="<?php echo $BaseURL; ?>">
+	<div class="sidebar">
+		<div class="header-link">
+			<a href="<?php echo $BaseURL; ?>"><?php echo $Project; ?> API</a>
+		</div>
+		
+		<input class="form-control typeahead" type="text" placeholder="Search functions">
+		
+		<noscript>
+			<style>
+				.typeahead {
+					display: none;
+				}
+				
+				.bg-primary {
+					padding: 10px;
+					text-align: center;
+				}
+			</style>
+			
+			<p class="bg-primary">Search requires javascript to work</p>
+		</noscript>
+		
+		<?php require __DIR__ . '/sidebar.php'; ?>
+	</div>
+	
 	<div class="container-fluid">
 		<div class="row">
-			<div class="col-sm-3 col-md-2 sidebar">
-				<div class="header-link">
-					<a href="<?php echo $BaseURL; ?>"><?php echo $Project; ?> API</a>
-				</div>
-				
-				<input class="form-control typeahead" type="text" placeholder="Search functions">
-				
-				<noscript>
-					<style>
-						.typeahead {
-							display: none;
-						}
-						
-						.bg-primary {
-							padding: 10px;
-							text-align: center;
-						}
-					</style>
-					
-					<p class="bg-primary">Search requires javascript to work</p>
-				</noscript>
-				
-				<?php require __DIR__ . '/sidebar.php'; ?>
-			</div>
-			<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main" id="pjax-container">
+			<div class="col-lg-12" id="pjax-container">
 <?php else: ?>
 <title><?php echo $Title; ?></title>
 <?php endif; ?>
